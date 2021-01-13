eb3a21656e3ea930817823bedd57a63088685309
RavTiles <32435927+RavTiles@users.noreply.github.com>
2021-01-13 14:52:08 +0100

    Add files via upload

---
+++ claimboards_config.lua
@@ -0,0 +1,46 @@
+--[[
+  ______  __          ___       __  .___  ___.                         
+ /      ||  |        /   \     |  | |   \/   |                         
+|  ,----'|  |       /  ^  \    |  | |  \  /  |                         
+|  |     |  |      /  /_\  \   |  | |  |\/|  |                         
+|  `----.|  `----./  _____  \  |  | |  |  |  |                         
+ \______||_______/__/     \__\ |__| |__|  |__|                         
+                                                                       
+.______     ______        ___      .______       _______       _______.
+|   _  \   /  __  \      /   \     |   _  \     |       \     /       |
+|  |_)  | |  |  |  |    /  ^  \    |  |_)  |    |  .--.  |   |   (----`
+|   _  <  |  |  |  |   /  /_\  \   |      /     |  |  |  |    \   \    
+|  |_)  | |  `--'  |  /  _____  \  |  |\  \----.|  '--'  |.----)   |   
+|______/   \______/  /__/     \__\ | _| `._____||_______/ |_______/    
+                                                                       
+	PepeshiRDM (STEAM_0:0:93705558) copyright			    2021
+		S H A R E D 		C O N F I G
+
+	This file contains all configurable things for the Claim Boards
+]]
+
+CB.JOBS= {
+	["Mayor"] = true,
+	["Civil Protection Chief"] = true
+}
+
+CB.LANG = {
+	["CLAIM"] = "Claim",
+	["UNCLAIM"] = "Unclaim",
+	["DESC"] = "Description:",
+	["UNCLAIMED"] = "Unclaimed",
+	["WITHOUTPREMISSIONS"] = "You don't have permissions to use that!",
+	["ALREADYCLAIMED"] = "This board is already climed by",
+	["SUCESSCLAIM"] = "Successfully claimed board",
+	["SUCESSUNCLAIM"] = "Successfully unclaimed board",
+	["UPDATE"] = "Update Description",
+	["SUCESSUPDATE"] = "Sucessfully updated description"
+}
+
+CB.COLORS = {
+	["CHAT"] = Color(255, 255, 255),
+	["TOP"] = Color(50, 50, 50),
+	["BOTTOM"] = Color(80, 80, 80),
+	["FRAME"] = Color(153, 129, 0),
+	["FONT"] = Color(255, 255, 255)
+}
